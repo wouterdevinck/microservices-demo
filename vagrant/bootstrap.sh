ddda3e95eeebdbe6dde0059b8774666f1fd8ed74
Wouter Devinck <wouter.devinck@barco.com>
2018-01-30 20:21:09 +0100

    Improvements and documentation

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -9,7 +9,11 @@ NEWUSERNAME="student"
 adduser --disabled-password --gecos "" $NEWUSERNAME
 sudo passwd -d $NEWUSERNAME
 sudo usermod -aG sudo $NEWUSERNAME
+echo "$NEWUSERNAME ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo
 sudo deluser --remove-home ubuntu
+
+# Edit hosts
+echo "127.0.1.1 ubuntu ubuntu" | sudo tee -a /etc/hosts
 
 # Update package source
 sudo apt-get update
@@ -46,9 +50,6 @@ sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode s
 sudo apt-get update
 sudo apt-get install -y code
 
-# Install relevant Visual Studio Code extensions
-# code --user-data-dir=/home/$NEWUSERNAME --install-extension PeterJausovec.vscode-docker
-
 # Install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
@@ -63,9 +64,43 @@ wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
 sudo tar -xzf postman.tar.gz -C /opt
 rm postman.tar.gz
 sudo ln -s /opt/Postman/Postman /usr/bin/postman
+mkdir -p /home/$NEWUSERNAME/.local/share/applications/
+cat > /home/$NEWUSERNAME/.local/share/applications/postman.desktop <<EOL
+[Desktop Entry]
+Encoding=UTF-8
+Name=Postman
+Exec=postman
+Icon=/opt/Postman/resources/app/assets/icon.png
+Terminal=false
+Type=Application
+Categories=Development;
+EOL
 
-# Uninstall LibreOffice to reclaim some disk space
+# Install Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update 
+sudo apt-get install google-chrome-stable
+
+# Desktop shortcuts
+cp /usr/share/applications/gnome-terminal.desktop /home/$NEWUSERNAME/Desktop/
+cp /usr/share/applications/code.desktop /home/$NEWUSERNAME/Desktop/
+cp /usr/share/applications/google-chrome.desktop /home/$NEWUSERNAME/Desktop/
+cp /home/$NEWUSERNAME/.local/share/applications/postman.desktop /home/$NEWUSERNAME/Desktop/
+sudo chmod 750 -R /home/$NEWUSERNAME/Desktop/
+
+# Install relevant Visual Studio Code extensions
+echo "code --install-extension PeterJausovec.vscode-docker" >> /home/$NEWUSERNAME/.bashrc
+
+# Uninstall LibreOffice and other bloat to reclaim some disk space
 sudo apt-get remove -y --purge libreoffice*
+sudo apt-get remove -y --purge account-plugin-* 
+sudo apt-get remove -y --purge aisleriot brltty colord deja-dup deja-dup-backend-gvfs duplicity \
+  empathy empathy-common evolution-data-server-online-accounts example-content firefox \
+  gnome-accessibility-themes gnome-contacts gnome-mahjongg gnome-mines gnome-orca gnome-screensaver \
+  gnome-sudoku gnome-video-effects gnomine landscape-common libsane libsane-common mcp-account-manager-uoa \
+  python3-uno rhythmbox rhythmbox-* sane-utils shotwell shotwell-common telepathy-* thunderbird thunderbird-* \
+  totem totem-* unity-scope-* unity-webapps-common
 sudo apt-get clean -y
 sudo apt-get autoremove -y
 
