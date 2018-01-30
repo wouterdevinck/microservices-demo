f271ad80c81eebc76300edf9770bb3792fd1a053
Wouter Devinck <wouter.devinck@barco.com>
2018-01-30 21:22:20 +0100

    Get MongoDB url from environment

--- service-task-api/server.ts
+++ service-task-api/server.ts
@@ -3,8 +3,12 @@ import * as mongoose from "mongoose"
 import * as bodyParser from "body-parser"
 import { TaskController } from "./taskcontroller"
 
-let mongo = 'mongodb://127.0.0.1/tasks'
-let base = '/api/v1/'
+const baseUrl = '/api/v1/'
+const dbUrl = process.env.DB_URL
+
+if (dbUrl === undefined) {
+  throw new Error('DB_URL not defined in environment')
+}
 
 const app = express()
 app.use(bodyParser.json())
@@ -12,7 +16,7 @@ app.use(bodyParser.json())
 app.listen(4200, () => {
   console.log('[INFO] Web server listening on port 4200...')
 
-  mongoose.connect(mongo, (err) => {
+  mongoose.connect(dbUrl, (err) => {
     if (err) {
       console.log('[ERROR]' + err.message)
       console.log('[ERROR]' + err)
@@ -20,8 +24,8 @@ app.listen(4200, () => {
       console.log('[INFO] Connected to MongoDB')
       
       let controller = new TaskController()
-      app.get(base + 'tasks', controller.GetAll)
-      app.put(base + 'tasks', controller.Create)
+      app.get(baseUrl + 'tasks', controller.GetAll)
+      app.put(baseUrl + 'tasks', controller.Create)
 
     }
   })
