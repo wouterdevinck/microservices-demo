fec838a61b9e42f7a38af58f5661be8d07108323
Wouter Devinck <wouter.devinck@barco.com>
2018-01-30 20:41:47 +0100

    Fix

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -80,7 +80,7 @@ EOL
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update 
-sudo apt-get install google-chrome-stable
+sudo apt-get install -y google-chrome-stable
 
 # Desktop shortcuts
 cp /usr/share/applications/gnome-terminal.desktop /home/$NEWUSERNAME/Desktop/
