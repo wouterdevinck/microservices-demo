fe16c1f8ee7d584eb630d65bead3f5db1ac1bb45
Wouter Devinck <woutr.devinck@gmail.com>
2018-02-06 02:19:21 +0100

    Ugly temp workaround for webpack issue

--- service-frontend/src/app.component.ts
+++ service-frontend/src/app.component.ts
@@ -4,8 +4,79 @@ import { TaskModel } from './taskmodel'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: [ './styles.css' ],
+  //templateUrl: './app.component.html',
+  //styleUrls: [ './styles.css' ],
+  template: ' \
+  <div class="container"> \
+  \
+    <div class="py-5 text-center"> \
+      <i class="material-icons logo">done_all</i> \
+      <h2>Task list app</h2> \
+    </div> \
+    \
+    <ul class="list-group"> \
+      <li class="list-group-item list-group-item-action"  \
+        *ngFor="let task of tasks" (click)="check(task._id)"> \
+        <div class="custom-control custom-checkbox"> \
+          <input type="checkbox" class="custom-control-input"  \
+            id="chk{{ task._id }}" [checked]="task.done" \
+            (change)="check(task._id, $event)" disabled> \
+          <label class="custom-control-label" for="chk{{ task._id }}"  \
+            [ngClass]="{\'done\': task.done}"> \
+            {{ task.description }} \
+          </label> \
+          <i class="material-icons delete" (click)="delete(task._id)">delete</i> \
+        </div> \
+      </li>   \
+      <li class="list-group-item"> \
+        <input type="text" class="form-control" placeholder="Create new task..."  \
+          (keypress)="keypress($event)" [(ngModel)]="newTask"> \
+      </li> \
+    </ul> \
+    \
+    <footer class="my-5 text-muted text-center text-small"> \
+      <p class="mb-1">&copy; 2018 Wouter Devinck</p> \
+      <ul class="list-inline"> \
+        <li class="list-inline-item"><a href="https://github.com/wouterdevinck/microservices-demo">Source code</a></li> \
+        <li class="list-inline-item">-</li> \
+        <li class="list-inline-item"><a href="https://www.apache.org/licenses/LICENSE-2.0">License</a></li> \
+      </ul> \
+    </footer> \
+    \
+  </div>  \
+    ',
+    styles: ['  \
+    .logo {   \
+      font-size: 72px;   \
+      color: darkblue;   \
+    }  \
+    .container {  \
+      max-width: 800px;  \
+    }  \
+    .done {  \
+      text-decoration: line-through;  \
+    }  \
+    .custom-control-label {  \
+      margin-left: 8px;  \
+      padding-top: 1px;  \
+    }  \
+    .custom-control-input:disabled~.custom-control-label {  \
+      color: black;  \
+    }  \
+    .delete {  \
+      float: right;  \
+      font-size: 1.3em;  \
+      padding-top: 4px;  \
+      color: lightgrey;  \
+    }  \
+    .delete:hover {  \
+      color: red;  \
+      cursor: pointer;  \
+    }  \
+    .text-small {  \
+      font-size: x-small;  \
+    }  \
+    '],
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
