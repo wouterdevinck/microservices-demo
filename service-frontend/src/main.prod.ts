fc998b74778a880a796278919424337853b5ceb2
Wouter Devinck <wouter.devinck@gmail.com>
2018-02-08 00:28:13 +0100

    Fix & cleanup

--- service-frontend/src/main.prod.ts
+++ service-frontend/src/main.prod.ts
@@ -1,19 +1,9 @@
-/*require('zone.js/dist/zone')
+require('zone.js/dist/zone')
 import { platformBrowser } from '@angular/platform-browser'
 import { enableProdMode } from '@angular/core'
 
-import { AppModuleNgFactory } from './app/app.module.ngfactory'
+import { AppModuleNgFactory } from './app.module.ngfactory'
 
 enableProdMode()
 
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory)*/
-
-
-require('zone.js/dist/zone')
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
-import { enableProdMode } from '@angular/core'
-import { AppModule } from './app.module'
-
-enableProdMode()
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory)
