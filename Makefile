c690958fd9621cefd5493830986e6d5e53565674
Wouter Devinck <wouter.devinck@gmail.com>
2018-02-07 14:48:15 +0100

    Deploy system updates

--- Makefile
+++ Makefile
@@ -1,27 +1,27 @@
 ######################### BUILDING & RUNNING #########################
 
-# "make" - build all Docker images
+# "make" - Build all Docker images.
 .PHONY: all
 all: build build-dev
 
-# "make start" - build and run the *development* Docker images
+# "make start" - Build and run the *development* Docker images.
 .PHONY: start
 start: build-dev up-dev
 
-# "make serve" - build and run the *final* Docker images
+# "make serve" - Build and run the *final* (i.e. "production", optimized) Docker images.
 .PHONY: serve
 serve: build up
 
-# "make clean" - stop all Dockers and clean up
+# "make clean" - Stop all Dockers and clean up.
 .PHONY: clean
 clean: stop
 	docker system prune -f
 	
-# "make stop" - stop all Dockers
+# "make stop" - Stop all Docker containers.
 .PHONY: stop
 stop: down down-dev
 
-# Building and running the *final* Docker images using Compose
+# Building and running the *final* Docker images using Compose.
 #   uses docker-compose.yml
 .PHONY: build up down
 build:
@@ -31,7 +31,7 @@ up:
 down:
 	docker-compose down
 
-# Building and running the *development* Docker images using Compose
+# Building and running the *development* Docker images using Compose.
 #   uses docker-compose-dev.yml
 .PHONY: build-dev up-dev down-dev
 build-dev:
@@ -43,14 +43,14 @@ down-dev:
 
 ######################### DEVELOPMENT HELPERS #########################
 
-# Get an interactive shell in one of the running Docker
+# Get an interactive shell in one of the running Docker containers.
 #   make shell <docker-name>
 #   e.g. "make shell task-api"
 .PHONY: shell
 shell:
 	docker exec -it $(CMDARGS) /bin/sh
 
-# Execute a shell command in one of the running Dockers
+# Execute a shell command in one of the running Docker containers.
 #   make exec <docker-name> <command>
 #   e.g. "make exec task-api npm version"
 .PHONY: exec
@@ -66,20 +66,34 @@ ifeq ($(CMD),$(filter $(CMD),exec shell))
 endif
 
 ######################### KUBERNETES #########################
-# TODO These targets currently use a bash script - write a PowerShell version for Windows
+# These targets currently use a bash script, TODO write a PowerShell version for Windows.
 
-# "make login" - download token and certificate to talk to Kubernetes 
-#   cluster from bastion and configure kubectl to use these secrets
+# "make login" - Secure copy secrets from bastion and configures kubectl and docker.
 .PHONY: login
 login:
 	./k8s/kube.sh login
 
-# "make deploy" - deploy all services to Kubernetes cluster
+# "make logout" - Forget all secrets and unconfigure kubectl and docker.
+.PHONY: logout
+logout:
+	./k8s/kube.sh logout
+
+# "make publish" - Publish all docker images to Docker Hub.
+.PHONY: publish
+publish:
+	./k8s/kube.sh publish
+
+# "make deploy" - Deploy all services to the Kubernetes cluster.
 .PHONY: deploy
 deploy:
 	./k8s/kube.sh deploy
 
-# "make destroy" - delete all services from Kubernetes cluster
+# "make destroy" - Delete all services from the Kubernetes cluster.
 .PHONY: destroy
 destroy:
 	./k8s/kube.sh destroy
+
+# "make dashboard" - Open the Kubernetes dashboard.
+.PHONY: dashboard
+dashboard:
+	./k8s/kube.sh dashboard
--- k8s/kube.sh
+++ k8s/kube.sh
@@ -1,42 +1,51 @@
 #!/bin/bash
 set -e
 
-CLUSTER="k8s.me"
+CLUSTER="k8s.me"   # Domain name of Kubernetes cluster (assumes api.$CLUSER, bastion.$CLUSTER and dashboard.$CLUSTER)
+CONFIG="task-app"  # Name of template for Kubernetes yaml (defines services, deployments, etc.)
+LIST="images.list" # Name of file with list of images that need to be published to Docker Hub
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 CRED_DIR="$SCRIPT_DIR/../.credentials"
 
 function printUsage {
-  echo "Usage: kube.sh login|logout|publish|deploy|destroy"
-  # TODO more info
+  echo "Usage: $0 login|logout|publish|deploy|destroy|dashboard"
+  echo ""
+  echo "   login     - Secure copy secrets from bastion and configures kubectl and docker."
+  echo "   logout    - Forget all secrets and unconfigure kubectl and docker."
+  echo "   publish   - Publish all docker images to Docker Hub."
+  echo "   deploy    - Deploy all services to the Kubernetes cluster."
+  echo "   destroy   - Delete all services from the Kubernetes cluster."
+  echo "   dashboard - Open the Kubernetes dashboard."
+  echo ""
 }
 
 case $1 in
 
 "login") 
 
-  # 1. Ask for username/password
+  # Ask for username/password
   printf "Username: "
   read USERNAME
   printf "Password: "
   read -s PASSWORD
   printf "\n"
 
-  # 2. Store credentials
+  # Store credentials
   rm -rf $CRED_DIR || true
   mkdir $CRED_DIR
   echo "$USERNAME:$PASSWORD" > $CRED_DIR/account
 
-  # 3. Get certificate, token, etc. from bastion
+  # Get certificate, token, etc. from bastion
   sshpass -p "$PASSWORD" scp $USERNAME@bastion.$CLUSTER:$USERNAME.* $CRED_DIR
 
-  # 4. Configure kubectl (to talk to the Kubernetes cluster)
+  # Configure kubectl (to talk to the Kubernetes cluster)
   kubectl config set-cluster $CLUSTER-$USERNAME --embed-certs=true --server=https://api.$CLUSTER/ --certificate-authority=$CRED_DIR/$USERNAME.crt
   kubectl config set-credentials $USERNAME --token=`cat $CRED_DIR/$USERNAME.token`
   kubectl config set-context $USERNAME --cluster=$CLUSTER-$USERNAME --user=$USERNAME --namespace=$USERNAME
   kubectl config use-context $USERNAME
 
-  # 5. Configure docker (to push images to Docker Hub)
+  # Configure docker (to push images to Docker Hub)
   HUB=`cat $CRED_DIR/$USERNAME.hub`
   HUBCRED=(${HUB//:/ })
   echo "${HUBCRED[1]}" | docker login --username ${HUBCRED[0]} --password-stdin
@@ -45,43 +54,101 @@ case $1 in
 
 "logout")
 
-  # 1. Logout from Kubernetes
+  # Logout from Kubernetes
   rm -f ~/.kube/config || true
 
-  # 2. Logout from Docker Hub
+  # Logout from Docker Hub
   docker logout
 
-  # 3. Remove credential from disk
+  # Remove credential from disk
   rm -rf $CRED_DIR || true
 
   ;;
 
 "publish")
 
-  # TODO
+  if [ ! -d "$CRED_DIR" ]; then
+    echo "Please log in first."
+    exit 2
+  fi
+
+  # Read username and docker hub username
+  ACCOUNT=`cat $CRED_DIR/account`
+  CRED=(${ACCOUNT//:/ })
+  USERNAME=${CRED[0]}
+  HUB=`cat $CRED_DIR/$USERNAME.hub`
+  HUBCRED=(${HUB//:/ })
+  HUBACCOUNT=${HUBCRED[0]}
+
+  # Load the list of images that need to be pushed to Docker hub
+  while IFS='' read -r IMG || [[ -n "$IMG" ]]; do
+
+    # Tag image with unique name and push it to Docker Hub
+    TAG="$HUBACCOUNT/$IMG-$USERNAME"
+    echo "Pushing image $IMG as $TAG"
+    docker tag $IMG $TAG
+    docker push $TAG
+
+  done < "$SCRIPT_DIR/$LIST"
 
   ;;
 
 "deploy"|"destroy")
 
-  # 1. Load stored credentials
+  if [ ! -d "$CRED_DIR" ]; then
+    echo "Please log in first."
+    exit 2
+  fi
+
+  # Load stored credentials & settings into environment variables
   ACCOUNT=`cat $CRED_DIR/account`
   CRED=(${ACCOUNT//:/ })
   export USERNAME=${CRED[0]}
   export PASSWORD=${CRED[1]}
+  HUB=`cat $CRED_DIR/$USERNAME.hub`
+  HUBCRED=(${HUB//:/ })
+  export HUBACCOUNT=${HUBCRED[0]}
 
-  # 2. Transform the template by substituting the environment variables
-  envsubst < $SCRIPT_DIR/task-app.tmpl > $SCRIPT_DIR/task-app.yaml
+  # Transform the template by substituting the environment variables
+  envsubst < $SCRIPT_DIR/$CONFIG.tmpl > $SCRIPT_DIR/$CONFIG.yaml
 
-  # 3. Deploy/delete this config to/from the Kubernetes cluster
+  # Deploy/delete this config to/from the Kubernetes cluster
   if [ $1 = "deploy" ]; then
-    kubectl apply -f $SCRIPT_DIR/task-app.yaml
+    kubectl apply -f $SCRIPT_DIR/$CONFIG.yaml
+
+    # Print url
+    URL=`cat $SCRIPT_DIR/$CONFIG.yaml | grep 'host' | sed 's/- host://' | sed 's/ //g'`
+    printf "\nOnce DNS propagation has completed, your service should become available on: \n  https://$URL\n\n"
+
   else
-    kubectl delete -f $SCRIPT_DIR/task-app.yaml
+    kubectl delete -f $SCRIPT_DIR/$CONFIG.yaml
   fi
   
-  # 4. Clean up
-  rm $SCRIPT_DIR/task-app.yaml
+  # Clean up
+  rm $SCRIPT_DIR/$CONFIG.yaml
+
+  ;;
+
+"dashboard")
+
+  if [ ! -d "$CRED_DIR" ]; then
+    echo "Please log in first."
+    exit 2
+  fi
+
+  # Load stored username
+  ACCOUNT=`cat $CRED_DIR/account`
+  CRED=(${ACCOUNT//:/ })
+  USERNAME=${CRED[0]}
+
+  # Show token and open browser
+  URL="https://dashboard.$CLUSTER#!/overview?namespace=$USERNAME"
+  printf "\nUse the following token to sign in on the dashboard:\n\n"
+  cat $CRED_DIR/$USERNAME.token
+  printf "\nAnd select the namespace \"$USERNAME\" on the left.\n"
+  printf "\nPress [enter] to open $URL in your default browser"
+  read
+  xdg-open $URL > /dev/null 2>&1
 
   ;;
 
